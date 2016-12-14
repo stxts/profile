03fb670009e34a85f738c591a55a4d79a0f7298b
Songtham Tungkitkancharoen <songthamtung@gmail.com>
2016-12-13 21:14:21 -0800

    moved googlemaps to application.js + some css

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -51,3 +51,26 @@ $(document).ready(function(){
     });
   });
 })
+
+
+var myCenter = new google.maps.LatLng(37.7749, -122.4194);
+
+function initialize() {
+var mapProp = {
+center:myCenter,
+zoom:12,
+scrollwheel:false,
+draggable:false,
+mapTypeId:google.maps.MapTypeId.ROADMAP
+};
+
+var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
+
+var marker = new google.maps.Marker({
+position:myCenter,
+});
+
+marker.setMap(map);
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);

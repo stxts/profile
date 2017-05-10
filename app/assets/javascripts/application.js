4e62b8de3b562519b836786fa920b0a80fa351d3
Songtham Tungkitkancharoen <songthamtung@gmail.com>
2017-05-10 00:33:52 -0700

    added jQuery terminal

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -93,3 +93,58 @@ ga('send', 'pageview');
 $(document).ready(function(){
     $('[data-toggle="tooltip"]').tooltip();   
 });
+
+
+jQuery(function($, undefined) {
+    $('#st_terminal').terminal(function(command) {
+          if (command == 'about') {
+              this.echo("Name: Songtham Tungkitkancharoen\nAge: 25\nCity: San Francisco\nSchool: UC San Diego '14\nDegree: B.A. Sociology w/ High Honors\nOccupation: Software Engineer\nTech: C# .NET, AngularJS, Ruby on Rails, Python, SQL");
+          }
+          else if (command == 'who') {
+              this.echo("Songtham Tungkitkancharoen");
+          }
+          else if (command == 'what') {
+              this.echo("Full Stack Web Developer");
+          }
+          else if (command == 'where') {
+              this.echo("San Francisco");
+          }
+          else if (command == 'when') {
+              this.echo("2+ years experience");
+          }
+          else if (command == 'why') {
+              this.echo("I like to create and solve problems. Oh and coding is really fun :)");
+          }
+          else if (command == 'how') {
+              this.echo("Self Taught");
+          }
+          else if (command == 'books') {
+              this.echo("Shantaram — Gregory David Roberts\nThe Four Agreements — Don Miguel Ruiz\nThe Alchemist — Paulo Coelho\nTao of Wu — RZA");
+          }
+          else if (command == 'quotes') {
+              this.echo("'The journey of a thousand miles begins with a single step' — Lao Tzu");
+          }
+          else if (command == 'help') {
+              this.echo("[[b;#1abc9c;<BACKGROUND>;]about] — summary\n[[b;#1abc9c;<BACKGROUND>;]who] — I am\n[[b;#1abc9c;<BACKGROUND>;]what] — I do\n[[b;#1abc9c;<BACKGROUND>;]where] — I live\n[[b;#1abc9c;<BACKGROUND>;]when] — I started\n[[b;#1abc9c;<BACKGROUND>;]why] — I code\n[[b;#1abc9c;<BACKGROUND>;]how] — I learned\n[[b;#1abc9c;<BACKGROUND>;]books] — a list of some of my favorite books\n[[b;#1abc9c;<BACKGROUND>;]quotes] — a list of some of my favorite quotes\n[[b;#1abc9c;<BACKGROUND>;]1+1] — built-in calculator\n[[b;#1abc9c;<BACKGROUND>;]clear] — clear terminal");
+          }
+           else if (command !== '') {
+            try {
+                var result = window.eval(command);
+                if (result !== undefined) {
+                    this.echo(new String(result));
+                }
+            } catch(e) {
+                this.error(new String(e));
+                this.echo(new String("Type [[b;#1abc9c;<BACKGROUND>;]help] to see a list of available commands"));
+            }
+        } else {
+           this.echo('');
+        }
+    }, {
+        greetings: 'Welcome to www.songthamtung.com ~\nType [[b;#1abc9c;<BACKGROUND>;]about] to get started!',
+        name: 'st_terminal',
+        height: 200,
+        prompt: '>_ST ',
+        convertLinks: false
+    });
+});
